a95e33277d0328dae841396aa3b98d9072d7d112
gianca2020 <giancarloforero@gmail.com>
2026-01-08 02:44:03 -0500

    Add social media icons to footer

---
+++ components/layout/Footer.jsx
@@ -0,0 +1,14 @@
+import Icons from "../ui/Icons";
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-8 mt-12">
+      <Icons />
+      <p className="text-center text-gray-400 text-sm mt-6">
+        © 2025 Nikole Rajgor. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
