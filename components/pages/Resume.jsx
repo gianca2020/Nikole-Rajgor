88d09188f5f87ee7e23857701a2302b7141d1bcb
gianca2020 <giancarloforero@gmail.com>
2026-01-09 00:10:48 -0500

    ???

--- components/pages/Resume.jsx
+++ components/pages/Resume.jsx
@@ -30,7 +30,7 @@ const Resume = () => {
         <header className="mt-6 mb-12 flex flex-col items-center text-center">
           <div className="relative">
             <h1 className="text-8xl md:text-9xl tracking-normal leading-[1.5]" style={{ fontFamily: 'Italianno, cursive', letterSpacing: '0.08em', fontWeight: '50', color: '#F6F1E8' }}>Resume</h1>
-            <div className="absolute bottom-14 left-1/2 transform -translate-x-1/2 w-80 h-px bg-[#F6F1E8] header-line"></div>
+            <div className="absolute bottom-10 md:bottom-14 left-1/2 transform -translate-x-1/2 w-80 h-px bg-[#F6F1E8] header-line"></div>
           </div>
         </header>
 
